6a1d94ef597793d70e339a73c97e1728c93a618f
Larry Barrios Jr. <github@larrybarriosjr.com>
2025-10-18 20:07:26 +0800

    Add env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import biome from "vite-plugin-biome"
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), biome()],
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "")
+
+	return {
+		plugins: [react(), biome()],
+		server: {
+			open: true,
+			proxy: {
+				"/api": env.VITE_API_URL || "http://localhost:4289",
+			},
+		},
+	}
 })
