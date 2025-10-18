7b5abc3e72a62e94bfca368347af632daef6e026
Larry Barrios Jr. <github@larrybarriosjr.com>
2025-10-18 20:09:30 +0800

    Replace vite biome config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import "@mantine/carousel/styles.css"
 import "@mantine/core/styles.css"
+
+// @mantine/core must be imported first
+import "@mantine/carousel/styles.css"
 import "@mantine/notifications/styles.css"
 
 import { MantineProvider } from "@mantine/core"
+import { ModalsProvider } from "@mantine/modals"
+import { Notifications } from "@mantine/notifications"
 
 export default function App() {
-	return <MantineProvider></MantineProvider>
+	return (
+		<MantineProvider>
+			<ModalsProvider>
+				<Notifications />
+			</ModalsProvider>
+		</MantineProvider>
+	)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import { biomePlugin as biome } from "@pbr1111/vite-plugin-biome"
 import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from "vite"
-import biome from "vite-plugin-biome"
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
