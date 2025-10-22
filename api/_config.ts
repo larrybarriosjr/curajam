c8cd661ac024af518c0492906f3b8c90c03e0f18
Larry Barrios Jr. <github@larrybarriosjr.com>
2025-10-23 01:35:37 +0800

    Setup vitest

--- api/_config.ts
+++
@@ -1,7 +0,0 @@
-import { Innertube, UniversalCache } from "youtubei.js"
-
-// IIFE to replicate top-level await
-export const IT = await (async () =>
-	await Innertube.create({
-		cache: new UniversalCache(true),
-	}))()
