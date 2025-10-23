5dab3273861815af4cc2914e361f47345f87cd04
Larry Barrios Jr. <github@larrybarriosjr.com>
2025-10-24 00:15:29 +0800

    Move non-build outputs to __local__

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 import sonda from "sonda/vite"
 import { defineConfig, loadEnv } from "vite"
+import "vitest/config"
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -27,20 +28,23 @@ export default defineConfig(({ mode }) => {
 			sourcemap: true,
 		},
 		plugins: [
+			sonda({ outputDir: "__local__/.sonda" }),
 			tanstackRouter({
 				autoCodeSplitting: true,
 				generatedRouteTree: "./src/app/routeTree.gen.ts",
 				target: "react",
+				tmpDir: "__local__/.tanstack/tmp",
 			}),
 			react(),
 			biome(),
-			sonda(),
 		],
 		resolve: {
 			alias: {
+				api: path.resolve(__dirname, "./api"),
 				app: path.resolve(__dirname, "./src/app"),
 				assets: path.resolve(__dirname, "./src/assets"),
 				components: path.resolve(__dirname, "./src/components"),
+				features: path.resolve(__dirname, "./src/features"),
 				hooks: path.resolve(__dirname, "./src/hooks"),
 			},
 		},
@@ -48,6 +52,11 @@ export default defineConfig(({ mode }) => {
 			open: true,
 			proxy: {
 				"/api": env.VITE_API_URL || "http://localhost:4289",
+			},
+		},
+		test: {
+			coverage: {
+				reportsDirectory: "__local__/coverage",
 			},
 		},
 	}
